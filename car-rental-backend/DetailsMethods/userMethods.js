33f2f68bcc5288e25b8e9b49366d2d7e0e35494e
ajay-keelu <20a91a05f6@aec.edu.in>
2024-01-06 19:52:13 +0530

    car rent app

--- car-rental-backend/DetailsMethods/userMethods.js
+++ car-rental-backend/DetailsMethods/userMethods.js
@@ -9,7 +9,6 @@ const authUserLogin = async (req, res) => {
         res.send("User not found")
     }
 }
-//signup
 const authUserSignup = async (req, res) => {
     const { email } = req.body;
     const data = await User.find({ email: email });
@@ -23,9 +22,6 @@ const authUserSignup = async (req, res) => {
         res.send('User already exists')
     }
 }
-
-
-// bookings post
 const UpdateBookings = async (req, res) => {
     Booking.insertMany({ owneremail: req.body.owneremail, useremail: req.body.useremail, details: req.body }).then((val) => {
         res.send('success')
@@ -33,8 +29,6 @@ const UpdateBookings = async (req, res) => {
         res.send('fail')
     })
 }
-
-// bookings get 
 const userBookings = async (req, res) => {
     const email = req.user
     if (email) {
@@ -44,9 +38,6 @@ const userBookings = async (req, res) => {
         res.send('no auth')
     }
 }
-
-
-// Payment post
 const UpdatePayments = async (req, res) => {
     Payment.insertMany({ owneremail: req.body.owneremail, useremail: req.body.useremail, details: req.body }).then((val) => {
         res.send('success')
@@ -54,20 +45,15 @@ const UpdatePayments = async (req, res) => {
         res.send('fail')
     })
 }
-
-//payment get
 const userPayments = async (req, res) => {
     const email = req.user
     if (email) {
         const data = await Payment.find({ useremail: email })
-        // console.log('data payment : ', data)
         res.send(data)
     } else {
         res.send('no auth')
     }
 }
-
-// get all cars to users
 
 const allCars = async (req, res) => {
     try {
